edc3b7cf8d831821b72512311cde6c915bd97d1a
YohanJirage <yjirage@gmail.com>
2024-02-05 13:13:46 +0530

    App created

---
+++ Bulky1/BulkyWeb1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyWeb1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
---
+++ Bulky1/BulkyWeb1/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyWeb1.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+
+
+    }
+}
